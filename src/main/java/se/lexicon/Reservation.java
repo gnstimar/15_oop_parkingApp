49e8a1d1db57b8db6f9a835cea1b0d745e9e70d5
Agnes Timar <timaragnes@outlook.hu>
2026-07-09 14:36:42 +0200

    Add calculateFee

--- src/main/java/se/lexicon/Reservation.java
+++ src/main/java/se/lexicon/Reservation.java
@@ -1,23 +1,46 @@
 package se.lexicon;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
 
 public class Reservation {
-    private final String UUID;
-    private ParkingSpot parkingSpot;
+    private final UUID uuid;
+    private int spotId;
     private Person person;
     private LocalDateTime arrivalTime;
-    private long parkingInterval;
+    private long durationMin;
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
-    public Reservation(ParkingSpot parkingSpot, Person person, LocalDateTime arrivalTime, long parkingInterval) {
-        this.UUID = java.util.UUID.randomUUID().toString();
-        this.parkingSpot = parkingSpot;
+    public Reservation(int spotId, Person person, LocalDateTime arrivalTime, long durationMin) {
+        this.uuid = java.util.UUID.randomUUID();
+        this.spotId = spotId;
         this.person = person;
         this.arrivalTime = arrivalTime;
-        this.parkingInterval = parkingInterval;
+        this.durationMin = durationMin;
     }
 
-    public LocalDateTime calculateEndTime() {
-        return null;
+    public Person getPerson() {
+        return person;
+    }
+
+    public long getDurationMin() {
+        return durationMin;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public int getSpotId() {
+        return spotId;
+    }
+
+    public String calculateEndTime() {
+        return arrivalTime.plusMinutes(durationMin).format(formatter);
+    }
+
+    public double calculateFee() {
+        return person.getPersonType().feePerHour() / 60 * durationMin;
     }
 }
